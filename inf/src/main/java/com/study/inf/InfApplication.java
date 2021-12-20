b2526548f850b201de45c89c7f8567be17af40b9
kangminseung1229 <whelming25@naver.com>
2021-12-20 10:54:23 +0900

    start

---
+++ inf/src/main/java/com/study/inf/InfApplication.java
@@ -0,0 +1,13 @@
+package com.study.inf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfApplication.class, args);
+	}
+
+}
