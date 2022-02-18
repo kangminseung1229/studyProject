72b4f6daa90f99bc5cf009b24f3e61fb0037d1bf
kangminseung1229 <whelming25@naver.com>
2022-02-18 17:41:39 +0900

    jsession 세션, 쿠키 rememberMe, npm version install

--- inf/src/main/java/com/study/inf/config/SecurityConfig.java
+++ inf/src/main/java/com/study/inf/config/SecurityConfig.java
@@ -1,47 +1,75 @@
 package com.study.inf.config;
 
+import javax.sql.DataSource;
+
+import com.study.inf.account.AccountService;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import lombok.RequiredArgsConstructor;
 
 /*
  * SecurityConfig
  */
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // TODO Auto-generated method stub
         http.authorizeRequests()
-            .mvcMatchers("/").permitAll()
-            .mvcMatchers("/home").permitAll()
-            .mvcMatchers("/", "/login","/sign-up", "/check-email-token",
-                "email-login", "check-email-login", "/login-link").permitAll()
-            .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
-            .anyRequest().authenticated();
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email-token",
+                        "email-login", "check-email-login", "/login-link")
+                .permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
 
-        //form login 기능 활성화
+        // form login 기능 활성화
         http.formLogin()
-            .loginPage("/login").permitAll();
+                .loginPage("/login").permitAll();
 
-        //logout 기능 활성화
+        // logout 기능 활성화
         http.logout()
-            .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/");
+
+        // 쿠키, 로그인
+        http.rememberMe()
+                .userDetailsService(accountService) // 사용자 정보 필요
+                .tokenRepository(tokenRepository()); //
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+
+        return jdbcTokenRepository;
+
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         // TODO Auto-generated method stub
-        //static folder는 시큐리티 체인에 해당되지 않는다.
+        // static folder는 시큐리티 체인에 해당되지 않는다.
         web.ignoring()
-            .mvcMatchers("/node_modules/**")
-            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 
     }
 }
