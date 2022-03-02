26a72f3a97ddaf4c390c1bbd355751189a2cf5c2
kangminseung1229 <whelming25@naver.com>
2022-03-02 11:28:20 +0900

    profile image update

--- inf/src/main/java/com/study/inf/settings/Profile.java
+++ inf/src/main/java/com/study/inf/settings/Profile.java
@@ -11,24 +11,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Profile {
 
-
-    @Length(max=50)
+    @Length(max = 50)
     private String bio;
-    
+
+    @Length(max = 50)
     private String url;
 
+    @Length(max = 50)
     private String occupation;
-    
+
+    @Length(max = 50)
     private String location;
 
-    public Profile(Account account){
+    private String profileImage;
+
+    public Profile(Account account) {
         this.bio = account.getBio();
         this.url = account.getUrl();
         this.occupation = account.getOccupation();
         this.location = account.getLocation();
+        this.profileImage = account.getProfileImage();
 
     }
 
-    
-    
 }
