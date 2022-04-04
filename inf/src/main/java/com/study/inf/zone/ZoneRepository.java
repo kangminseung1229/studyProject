bcd739111cd76087aaef0df348f8289b04a1017c
kangminseung1229 <whelming25@naver.com>
2022-04-04 11:43:56 +0900

    지역정보 csv 입력

---
+++ inf/src/main/java/com/study/inf/zone/ZoneRepository.java
@@ -0,0 +1,10 @@
+package com.study.inf.zone;
+
+import com.study.inf.domain.Zone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long>{
+    
+    
+}
