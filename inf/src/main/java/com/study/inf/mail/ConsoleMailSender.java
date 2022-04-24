d87c299ea364389cd94748683e1e7e59b241aba1
kangminseung1229 <73506074+kangminseung1229@users.noreply.github.com>
2022-04-24 12:19:29 +0900

    profile dev

--- inf/src/main/java/com/study/inf/mail/ConsoleMailSender.java
+++ inf/src/main/java/com/study/inf/mail/ConsoleMailSender.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
-@Profile("local")
+@Profile("dev")
 public class ConsoleMailSender implements JavaMailSender {
 
     @Override
